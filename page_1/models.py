b1e48371ee00f115f75ce7efe19448a446a497b5
Ray <raygabrielsantana@gmail.com>
2021-11-01 17:30:59 +0000

    first test for CV

---
+++ page_1/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.conf import settings
+
+class About_me():
+    pass
+
+class Education():
+    pass
+
+class Experience():
+    pass
+
+class Links():
+    pass
+
+
+
+
